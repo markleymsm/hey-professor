22820c673795394aacce9f30f00a7aee68b6798e
Markley Mendes <markley.mendes@gmail.com>
2023-11-16 20:25:07 -0300

    HEY-11: should be able to unlike a question and should be not able to unlike more than one time

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -13,7 +13,7 @@
       </button>
     </x-form>
     
-    <x-form :action="route('question.like', $question)">
+    <x-form :action="route('question.unlike', $question)">
       <button class="flex items-start space-x-1  text-red-500">
         <x-icons.tumbs-down class="w-5 h-5 hover:text-red-300 cursor-pointer"/>
         <span>{{$question->unlikes }}</span>
--- tests/Feature/VoteAQuestionTeste.php
+++ tests/Feature/VoteAQuestionTeste.php
@@ -35,3 +35,35 @@ it('should be not able to like more than one time', function () {
     expect($user->votes()->where('question_id', '=', $question->id)->get())
     ->toHaveCount(1);
 });
+
+it('should be able to unlike a question', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    post(route('question.unlike', $question))->assertRedirect();
+
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'user_id'     => $user->id,
+        'like'        => 0,
+        'unlike'      => 1,
+    ]);
+
+});
+
+it('should be not able to unlike more than one time', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+
+    expect($user->votes()->where('question_id', '=', $question->id)->get())
+    ->toHaveCount(1);
+});
