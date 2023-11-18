a5ac977994aff0ca8516a541c28da670385c551d
Markley Mendes <markley.mendes@gmail.com>
2023-11-18 14:03:54 -0300

    hey-14: it should make sure that only the person who has created the question can publish the question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Question;
 use Closure;
 use Illuminate\Http\{RedirectResponse, Request};
 
@@ -11,7 +10,7 @@ class QuestionController extends Controller
     public function store(): RedirectResponse
     {
 
-        $attibutes = request()->validate([
+        request()->validate([
             'question' => [
                 'required',
                 'min:10',
@@ -23,7 +22,7 @@ class QuestionController extends Controller
             ],
         ]);
 
-        Question::query()->create([
+        user()->questions()->create([
             'question' => request()->question,
             'draft'    => true,
         ]);
--- tests/Feature/Question/PublishTest.php
+++ tests/Feature/Question/PublishTest.php
@@ -7,7 +7,10 @@ use function Pest\Laravel\{actingAs, put};
 it('should be able to publish a question', function () {
     // Arrange - criar algumas perguntas
     $user     = User::factory()->create();
-    $question = Question::factory()->create(['draft' => false]);
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => false]);
+
     actingAs($user);
 
     // Act - acessar a rota
@@ -20,14 +23,18 @@ it('should be able to publish a question', function () {
 
 it('should make sure that only the person who has created the question can publish the question', function () {
     // Arrange - criar algumas perguntas
-    $user     = User::factory()->create();
-    $question = Question::factory()->create(['draft' => false]);
-    actingAs($user);
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
 
-    // Act - acessar a rota
+    actingAs($wrongUser);
+    put(route('question.publish', $question))->assertForbidden();
+
+    actingAs($rightUser);
     put(route('question.publish', $question))->assertRedirect();
-    $question->refresh();
 
-    // Assert - verificar se a lista de perguntas está sendo mostrada
-    expect($question)->draft->toBeFalse();
+    // $question->refresh();
+
+    // // Assert - verificar se a lista de perguntas está sendo mostrada
+    // expect($question)->draft->toBeFalse();
 });
