8053b18886e237b0268caa26ecb7a51f1190e8f7
Markley Mendes <markley.mendes@gmail.com>
2023-11-18 14:15:21 -0300

    hey-14: refactoring routes file to add auth middleware

--- tests/Feature/Question/CreateTest.php
+++ tests/Feature/Question/CreateTest.php
@@ -73,3 +73,10 @@ it('should create as a draft all the time', function () {
         'draft'    => true,
     ]);
 });
+
+test('only authenticated users can create a new question', function () {
+    post(route('question.store'), [
+        'question' => str_repeat('*', 10) . "?",
+    ])->assertRedirect(route('login'));
+
+});
