eb0c541e18229b9d5fc6f5de1e6f3a9f17695a82
Markley Mendes <markley.mendes@gmail.com>
2023-11-19 11:09:32 -0300

    hey-14: it should be able to list all question created by me

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Closure;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\{RedirectResponse, Request};
 
 class QuestionController extends Controller
 {
+    public function index(): View
+    {
+        return view('question.index', [
+            'questions' => user()->questions,
+        ]);
+    }
+
     public function store(): RedirectResponse
     {
 
@@ -27,6 +35,6 @@ class QuestionController extends Controller
             'draft'    => true,
         ]);
 
-        return to_route('dashboard');
+        return back();
     }
 }
