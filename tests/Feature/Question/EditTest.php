5b2ab22afc3d2c2478f741d60964d7fb374d4760
Markley Mendes <markley.mendes@gmail.com>
2023-11-23 09:57:10 -0300

    hey-16: it should return view

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -12,3 +12,12 @@ it('should be able to open a quetion to edit', function () {
 
     get(route('question.edit', $question))->assertSuccessful();
 });
+
+it('should return view', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->for($user, 'createdBy')->create();
+
+    actingAs($user);
+
+    get(route('question.edit', $question))->assertViewIs('question.edit');
+});
